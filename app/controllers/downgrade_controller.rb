20dda6197098e4e77925521e6b8b9b963d441948
Eljam Russell <eljamrussell@gmail.com>
2017-11-02 10:48:17 -0400

    completed

--- app/controllers/downgrade_controller.rb
+++ app/controllers/downgrade_controller.rb
@@ -6,6 +6,7 @@ class DowngradeController < ApplicationController
     current_user.standard!
     current_user.wikis.each do |pub|
       pub.update_attribute(:private, false)
+    end
 
     flash[:notice] = "Your account has been downgraded to a standard account. Your private wikis are now public. You will recieve a $15.00 refund."
     redirect_to root_path
